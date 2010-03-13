844cccbd56ec08de38bf56c533073cb2da2a4030
James Halliday <substack@gmail.com>
2010-03-13 14:09:24 -0900

    moved out routines into utility header

--- topside/topside.c
+++ topside/topside.c
@@ -1,25 +1,5 @@
 #include <pic.h>
-typedef unsigned char byte;
-
-// Set up magical safety stuff
-__IDLOC(1);
-__CONFIG(INTIO & WDTDIS & MCLRDIS & BORDIS & UNPROTECT & PWRTEN & LVPDIS);
-
-// wait for integer deciseconds
-void busywait_dsec(byte dsec) {
-    byte i,j,k;
-    for (i = dsec; i != 0; i--) // tenths of seconds
-        for (j = 100; j != 0; j--) // 10 ms = 0.01 sec
-            for (k = 250; k != 0; k--) NOP(); // 1 ms
-}
-
-// wait for integer centiseconds
-void busywait_csec(byte csec) {
-    byte i,j,k;
-    for (i = csec; i != 0; i--) // hundredths of seconds
-        for (j = 10; j != 0; j--) // 10 ms = 0.01 sec
-            for (k = 250; k != 0; k--) NOP(); // 1 ms
-}
+#include "util.h"
 
 void main() {
     TRISA = 0;
@@ -31,11 +11,6 @@ void main() {
     // No analog
     ANSEL = 0;
     
-    // Turn off interrupts
-    // perl -pe'($_)=/(\w+IE\b)/ and ($_="    $_ = 0;\n")' pic16f887.h
-    RBIE = RABIE = T0IE = PEIE = GIE = TMR1IE = TMR2IE = CCP1IE = SSPIE = TXIE =
-    RCIE = ADIE = CCP2IE = ULPWUIE = BCLIE = EEIE = C1IE = C2IE = OSFIE = 0;
-    
     // Everything off
     PORTA = 0x00;
     PORTB = 0x00;
@@ -43,11 +18,12 @@ void main() {
     PORTE = 0x00;
     PORTD = 0x00;
     
+    init();
+    
     while (1) {
         PORTA = (1 << 0) | (1 << 3) | (1 << 4);
-        busywait_csec(1);
+        wait_csec(1);
         PORTA = (1 << 1) | (1 << 2) | (1 << 5);
-        busywait_csec(1);
+        wait_csec(1);
     }
 }
-
---
+++ topside/util.h
@@ -0,0 +1,79 @@
+#ifndef PIC_UTIL_H
+#define PIC_UTIL_H
+#include <pic.h>
+
+typedef unsigned char byte;
+// Safety settings
+__IDLOC(1);
+__CONFIG(INTIO & WDTDIS & MCLRDIS & BORDIS & UNPROTECT & PWRTEN & LVPDIS);
+
+// wait for integer deciseconds
+void wait_dsec(byte dsec) {
+    byte i,j,k;
+    for (i = dsec; i != 0; i--) // tenths of seconds
+        for (j = 100; j != 0; j--) // 10 ms = 0.01 sec
+            for (k = 250; k != 0; k--) NOP(); // 1 ms
+}
+
+// wait for integer centiseconds
+void wait_csec(byte csec) {
+    byte i,j,k;
+    for (i = csec; i != 0; i--) // hundredths of seconds
+        for (j = 10; j != 0; j--) // 10 ms = 0.01 sec
+            for (k = 250; k != 0; k--) NOP(); // 1 ms
+}
+
+void serial_init(void) {
+    IRCF0 = IRCF1 = IRCF2 = 1;
+    BRG16=1;
+    BRGH=1; /* now baud rate will equal Fosc/(4*(n+1)) */
+    //SPBRG = 34; /* or 16 for 117kbps (at 8MHz internal clock) */
+    SPBRG = 34; /* or 16 for 117kbps (at 8MHz internal clock) */
+    SPBRGH=0; /* high 8 bits of baud divisor */
+    // async operation, RS-232 inversion
+    SYNC=0;
+    SPEN=1;
+    TXEN=1;
+    CREN=1;
+    SCKP=1; // RS-232 style data inversion
+    
+    ADDEN=0; /* don't do address detection */
+    ABDEN=0; /* don't do auto-baud-detect */
+}
+
+void init(void) {
+    // Turn off interrupts
+    // perl -pe'($_)=/(\w+IE\b)/ and ($_="    $_ = 0;\n")' pic16f887.h
+    RBIE = RABIE = T0IE = PEIE = GIE = TMR1IE = TMR2IE = CCP1IE = SSPIE = TXIE =
+    RCIE = ADIE = CCP2IE = ULPWUIE = BCLIE = EEIE = C1IE = C2IE = OSFIE = 0;
+    
+    serial_init();
+}
+
+void serial_tx(byte b) {
+    while (!TXIF) {PORTC=1;/* outgoing still busy */}
+    TX9D = 0; // no parity
+    TXREG = b;
+}
+
+//static bit rcv, should; /* parity bit */
+byte serial_rx(void) {
+    int count=0;
+    int r;
+    while (!RCIF) {}
+    
+    if (OERR) {
+        CREN=0; CREN=1;
+    }
+    if (FERR) {
+        int q = RCREG;
+        FERR = 0; /* framing error--e.g., cable unplugged */
+        //PORTE = ~0x1;
+    }
+    
+    //rcv=RX9D; /* extract parity bit first */
+    r=RCREG; /* pull received data */
+    return r;
+}
+
+#endif
