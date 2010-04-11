e1f20b0dfd0edff451458bb6c376e6460d518d30
James Halliday <substack@gmail.com>
2010-04-11 14:00:22 -0800

    compute bytes to send

--- control/ROV/Comm.hs
+++ control/ROV/Comm.hs
@@ -2,7 +2,7 @@ module ROV.Comm (
     Comm(..), newComm, setMotor, send
 ) where
 
-import Data.Bits ((.|.))
+import Data.Bits ((.|.),bit)
 import Data.Word (Word8)
 import qualified Data.Map as M
 
@@ -12,11 +12,11 @@ import System.Process (system)
 import Data.Binary.Put (runPut,putWord8)
 import Data.ByteString.Lazy (ByteString,hPut,hGet)
 
-import Control.Concurrent (MVar,newMVar,takeMVar,putMVar)
+import Control.Monad (replicateM)
+import System.Random (randomRIO)
 
 data Comm = Comm {
     commH :: File.Handle,
-    commT :: MVar Int,
     commMotors :: M.Map Motor Float
 }
 
@@ -27,9 +27,7 @@ newComm :: FilePath -> IO Comm
 newComm dev = do
     fh <- File.openFile dev File.ReadWriteMode
     system $ "stty raw clocal 57600 cs8 -parenb parodd cstopb -echo < " ++ dev
-    tVar <- newMVar 0
     return $ Comm {
-        commT = tVar,
         commH = fh,
         commMotors = M.fromList $ zip [MLeft,MRight,MVertical] (repeat 0)
     }
@@ -39,11 +37,24 @@ setMotor comm motor power =
     comm { commMotors = M.insert motor power (commMotors comm) }
 
 send :: Comm -> IO ()
-send comm@Comm{ commT = tVar, commH = fh } = do
-    t <- takeMVar tVar
-    hPut fh $ runPut (putWord8 $ motorByte comm t)
-    putMVar tVar (t + 1)
+send comm@Comm{ commH = fh } = do
+    rs <- replicateM 3 $ randomRIO (0,1)
+    hPut fh $ runPut (putWord8 $ motorByte comm rs)
 
-motorByte :: Comm -> Int -> Word8
-motorByte comm t = byte where
-    byte = undefined
+motorByte :: Comm -> [Float] -> Word8
+motorByte Comm{ commMotors = motors } rs = byte where
+    byte = foldl (.|.) 0
+        $ map (setBit . fst)
+        $ filter enoughPower
+        $ zip [MLeft,MRight,MVertical] rs
+    enoughPower :: (Motor,Float) -> Bool
+    enoughPower (m,r) = (abs $ motors M.! m) > r
+    
+    setBit :: Motor -> Word8
+    setBit m = bit $ case (m,(motors M.! m) > 0) of
+        (MLeft,True) -> 0
+        (MLeft,False) -> 1
+        (MRight,True) -> 2
+        (MRight,False) -> 3
+        (MVertical,True) -> 5
+        (MVertical,False) -> 4
