326ba59d27da2d9cd46be574269dc0dbaf7ff360
James Halliday <substack@gmail.com>
2010-06-14 03:43:27 -0800

    prints out fft frequency decomposition

--- control/Freq.hs
+++ control/Freq.hs
@@ -1,20 +1,24 @@
 module Main where
+
 import Sound.Alsa
-import DSP.Estimation.Frequency.Pisarenko (pisarenko)
-import Numeric.Transform.Fourier.FFT (ifft)
+import Numeric.Transform.Fourier.FFT (fft)
+import qualified Data.Array as Ax
 
 import Foreign (Ptr, Storable, mallocArray, peekArray)
 import Control.Monad (forever)
-import Data.Array (listArray)
+import Data.Complex (Complex(..),magnitude)
 
 import Control.Concurrent (forkIO, threadDelay)
 import Control.Concurrent.MVar
+
+import Data.List (sortBy)
+import Data.Function (on)
 
 type MicState = (Double,Double) -- frequency, volume
 
 main :: IO ()
 main = do
-    micV <- micThread 44100 4000
+    micV <- micThread 44100 10000
     forever $ do
         (rawVol, freq) <- readMVar micV
         print (rawVol,freq)
@@ -26,16 +30,25 @@ micThread sampleRate samples = do
         source = alsaSoundSource "plughw:0,0" soundFormat
         soundFormat = SoundFmt sampleRate :: SoundFmt Double
     buf <- mallocArray samples
+    
     mv <- newMVar (0,0)
     forkIO $ withSoundSource source $ \handle -> forever $ do
         takeMVar mv
         n <- soundSourceRead source handle buf samples
         rawSound <- peekArray n buf
         
-        let -- phase angle is in radians
+        let
             volume = (sum $ map abs rawSound) / fromIntegral n
-            phaseAngle = pisarenko (listArray (0,n-1) rawSound)
-            --phaseAngle = ifft (listArray (0,n-1) rawSound)
-            hz = phaseAngle / (2 * pi) * fromIntegral sampleRate
-        putMVar mv $ if isNaN hz then (0,0) else (hz,volume)
+            rawFreqs = map magnitude $ Ax.elems $ fft
+                $ Ax.listArray (0,n-1) $ map (:+ 0) rawSound
+            sampleStep = (fromIntegral sampleRate / fromIntegral samples)
+            freqs = takeWhile ((< 5000) . fst)
+                $ dropWhile ((< 1000) . fst)
+                $ zip (iterate (+ sampleStep) 0) rawFreqs
+        
+        print $ take 20 $ reverse $ sortBy (compare `on` snd) freqs
+        
+        putMVar mv (0,0)
+        -- putMVar mv $ if isNaN hz then (0,0) else (hz,volume)
+        return ()
     return mv
