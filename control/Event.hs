9020e272a908e355ac4b29c38db06cdac32475d9
James Halliday <substack@gmail.com>
2010-06-22 12:42:06 -0800

    thread pool for events

--- control/Event.hs
+++ control/Event.hs
@@ -4,36 +4,59 @@ import Control.Monad
 import Control.Applicative ((<$>))
 import Data.Maybe (catMaybes)
 import qualified Data.Time.Clock.POSIX as T
+import qualified Data.Map as M
+
+import Control.Concurrent (forkOS,yield)
+import Control.Concurrent.MVar
+
+type Events = M.Map Int Event
+data Status = Running | Ready
+    deriving Eq
 
 data Event = Event {
+    evStatus :: Status,
     evTime :: Double,
     evElapsed :: Double,
     evAction :: Event -> IO (Maybe Event)
 }
 
-runEvents :: [Event] -> IO ()
-runEvents evs = do
+runEvents :: Int -> [Event] -> IO ()
+runEvents threadPoolSize evs = do
     t0 <- fromRational . toRational <$> T.getPOSIXTime
-    runEvents' t0 evs
+    eVar <- newMVar $ M.fromList $ zip [0..] evs
+    replicateM_ (threadPoolSize - 1) (forkOS $ eventThread eVar t0)
+    -- also use the main thread:
+    eventThread eVar t0
 
-runEvents' :: Double -> [Event] -> IO ()
-runEvents' _ [] = return ()
-runEvents' t0 evs = do
+eventThread :: MVar Events -> Double -> IO ()
+eventThread eVar t0 = do
     now <- fromRational . toRational <$> T.getPOSIXTime
-    runEvents' t0 . catMaybes =<< mapM (runEvent (now - t0)) evs
+    evs <- readMVar eVar
+    mapM_ (runEvent eVar (now - t0)) $ M.keys evs
+    yield
+    unless (M.null evs) $ eventThread eVar t0
 
-runEvent :: Double -> Event -> IO (Maybe Event)
-runEvent elapsed ev@Event{ evTime = t, evElapsed = e, evAction = f }
-    = if (elapsed - e > t)
-        then f ev
-        else return (Just ev)
+runEvent :: MVar Events -> Double -> Int -> IO ()
+runEvent eVar elapsed eid = do
+    evs <- takeMVar eVar
+    let ev@Event{ evStatus = s, evElapsed = e, evTime = t } = evs M.! eid
+    if (s == Ready && elapsed - e > t)
+        then do
+            -- not already running and due to be run
+            putMVar eVar $ M.insert eid (ev { evStatus = Running }) evs
+            mEv <- evAction ev $ ev
+            case mEv of
+                Nothing -> modifyMVar_ eVar (return . M.delete eid)
+                Just e -> modifyMVar_ eVar (return . M.insert eid e)
+        else putMVar eVar evs
 
 setTimeout :: Double -> IO a -> [Event] -> [Event]
 setTimeout t f
     = (:) Event {
         evTime = t,
         evElapsed = 0,
-        evAction = \_ -> f >> return Nothing
+        evAction = \_ -> f >> return Nothing,
+        evStatus = Ready
     }
 
 setInterval :: Double -> IO a -> [Event] -> [Event]
@@ -43,5 +66,6 @@ setInterval t f
         evElapsed = 0,
         evAction = \ev@Event{ evElapsed = e } -> do
             f
-            return $ Just (ev { evElapsed = e + t })
+            return $ Just (ev { evElapsed = e + t, evStatus = Ready }),
+        evStatus = Ready
     }
