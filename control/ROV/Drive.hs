6102b913a607afb3181edc1baa55f2fbb01a5d01
James Halliday <substack@gmail.com>
2010-06-23 13:58:48 -0800

    square input values

--- control/ROV/Drive.hs
+++ control/ROV/Drive.hs
@@ -10,9 +10,12 @@ import qualified Data.Map as M
 drive :: Comm -> InputState -> IO Comm
 drive comm input
     = execROV comm $ do
-        ML $= lx + ly
-        MR $= -lx + ly
-        MV $= ry
+        let slx = if lx > 0 then 1 else -1
+            sly = if ly > 0 then 1 else -1
+            sry = if ry > 0 then 1 else -1
+        ML $= slx * (negate $ lx ** 2) + sly * (ly ** 2)
+        MR $= slx * (lx ** 2) + sly * (ly ** 2)
+        MV $= sry * (ry ** 2)
         when (button Button5) $ Pitch $+ 0.1
         when (button Button4) $ Pitch $- 0.1
         when (button ButtonL) $ Pinchers $+ 0.25
